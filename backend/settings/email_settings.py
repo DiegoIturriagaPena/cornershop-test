d06beb7776a8b654c6907c2742c02f95c64c9e30
Santiago Daimel Alvarez Reyes <sdalvarezreyes@outlook.com>
2020-03-16 09:07:03 -0300

    Update email_settings.py

--- backend/settings/email_settings.py
+++ backend/settings/email_settings.py
@@ -2,5 +2,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'backend.cornershop.test@gmail.com '
 DEFAULT_FROM_EMAIL = 'backend.cornershop.test@gmail.com '
-EMAIL_HOST_PASSWORD = 'backend.test'
+EMAIL_HOST_PASSWORD = 'xxxxxxxxx'
 EMAIL_PORT = 587
